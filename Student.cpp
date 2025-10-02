df9735d745f327b60715b111da8773032c458385
Lalithaaaa <lalithakantam@gmail.com>
2025-10-02 14:48:38 -0400

    p1 done44

--- Student.cpp
+++ Student.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Student.hpp"
+#include <string>
+#include <vector>
+
+void Student::addScore(int score) {
+   scores.push_back(score);
+}
+double Student::average() const {
+    double avg = 0.0;
+    // (returns average of all scores, or 0.0 if none)
+    if (scores.size() == 0) {
+        return avg;
+    }
+    for (const auto& score : scores) {
+        avg += score;
+    }
+
+    return avg / scores.size();
+}
+const std::string& Student::getName() const {
+    return name;
+}
+int Student::getId() const {
+    return id;
+}
--- Student.hpp
+++ Student.hpp
@@ -5,8 +5,21 @@
 #ifndef THE_FREAKING_EXAM_1_STUDENT_H
 #define THE_FREAKING_EXAM_1_STUDENT_H
 
+#include <string>
+#include <vector>
+
 
 class Student {
+public:
+    std::string name;
+    int id;
+    std::vector<int> scores;
+
+    void addScore(int score);
+    double average() const; // (returns average of all scores, or 0.0 if none)
+    const std::string& getName() const;
+    int getId() const;
+
 };
 
 

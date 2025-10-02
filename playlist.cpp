6331fe00253579960caaf246b5e4b50e47b055ad
Lalithaaaa <lalithakantam@gmail.com>
2025-10-02 12:29:47 -0400

    hpp

--- playlist.cpp
+++ playlist.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "playlist.hpp"
+
+playlist::playlist() {
+//numSongs_ to 0 and totalMinutes_ to 0.0.
+	numSongs_ = 0;
+	totalMinutes_ = 0.0;
+}
+
+playlist::playlist(int n, double t) {
+	numSongs_ = n;
+	totalMinutes_ = t;
+}
+
+playlist::playlist(int n, double t) {}
+
+setNumSongs(int n) {
+	numSongs_ = n;
+}
+
+playlist::setTotalMinutes(double t) {
+	totalMinutes = t;
+}
+
+int playlist::getNumSongs() const {
+	return numSongs_;
+}
+
+double playlist::getTotalMinutes() const {
+	return totalMinutes_;
+}
--- playlist.hpp
+++ playlist.hpp
@@ -8,9 +8,25 @@
 
 class playlist {
 
-int numSongs_; // number of songs in the playlist
-double totalMinutes_; // total play time of the playlist (in minutes)
+	public:
+	int numSongs_; // number of songs in the playlist
+	double totalMinutes_; // total play time of the playlist (in minutes)
 
+	//A default constructor that initializes numSongs_ to 0 and totalMinutes_ to 0.0.
+	playlist();
+
+	//A parameterized constructor that takes two arguments (int n, double t) and initializes accordingly.
+	playlist(int n, double t);
+
+	setNumSongs(int n); // sets the number of songs.
+
+	setTotalMinutes(double t); // sets the total time.
+
+	Getters (accessors);
+
+	getNumSongs() const; // returns the number of songs.
+
+	getTotalMinutes() const; // returns the total play time.
 
 };
 

8a6771a39aaa8feb4e9c62b4a7c0cfb8c0f5fc26
Lalithaaaa <lalithakantam@gmail.com>
2025-10-02 13:01:51 -0400

    capitols

--- Playlist.cpp
+++ Playlist.cpp
@@ -2,38 +2,36 @@
 // Created by lkant on 10/2/2025.
 //
 
-#include "playlist.hpp"
+#include "Playlist.hpp"
 
-playlist::playlist() {
+Playlist::Playlist() {
 //numSongs_ to 0 and totalMinutes_ to 0.0.
 	numSongs_ = 0;
 	totalMinutes_ = 0.0;
 }
 
-playlist::playlist(int n, double t) {
+Playlist::Playlist(int n, double t) {
 	numSongs_ = n;
 	totalMinutes_ = t;
 }
 
-playlist::playlist(int n, double t) {}
-
-setNumSongs(int n) {
+void Playlist::setNumSongs(int n) {
 	numSongs_ = n;
 }
 
-playlist::setTotalMinutes(double t) {
+void Playlist::setTotalMinutes(double t) {
 	totalMinutes = t;
 }
 
-int playlist::getNumSongs() const {
+int Playlist::getNumSongs() const {
 	return numSongs_;
 }
 
-double playlist::getTotalMinutes() const {
+double Playlist::getTotalMinutes() const {
 	return totalMinutes_;
 }
 
-int playlist::averageSongLength() const {
+int Playlist::averageSongLength() const {
 	// returns the average song length in minutes (totalMinutes_ / numSongs_). If numSongs_ is 0, return 0.
 	if(numSong_ == 0) {
 		return 0;
@@ -42,12 +40,12 @@ int playlist::averageSongLength() const {
 	return totalMinutes_ / numSongs_;
 }
 
-Playlist playlist::operator+(const Playlist& other) const {
-	playlist play = new playlist(other.getNumSongs(), other.getTotalMinutes());
+Playlist Playlist::operator+(const Playlist& other) const {
+	Playlist play = new Playlist(other.getNumSongs(), other.getTotalMinutes());
 	return play;
 }
 
-Playlist& playlist::operator+=(const Playlist& other) {
+Playlist& Playlist::operator+=(const Playlist& other) {
 	numSongs_ = other.getNumSongs();
 	totalMinutes_ = other.getTotalMinutes();
 }
--- Playlist.hpp
+++ Playlist.hpp
@@ -6,29 +6,27 @@
 #define THE_FREAKING_EXAM_1_PLAYLIST_H
 
 
-class playlist {
+class Playlist {
 
 	public:
 	int numSongs_; // number of songs in the playlist
 	double totalMinutes_; // total play time of the playlist (in minutes)
 
 	//A default constructor that initializes numSongs_ to 0 and totalMinutes_ to 0.0.
-	playlist();
+	Playlist();
 
 	//A parameterized constructor that takes two arguments (int n, double t) and initializes accordingly.
-	playlist(int n, double t);
+	Playlist(int n, double t);
 
-	setNumSongs(int n); // sets the number of songs.
+	void setNumSongs(int n); // sets the number of songs.
 
-	setTotalMinutes(double t); // sets the total time.
-
-	Getters (accessors);
+	void setTotalMinutes(double t); // sets the total time.
 
 	int getNumSongs() const; // returns the number of songs.
 
 	double getTotalMinutes() const; // returns the total play time.
 
-	averageSongLength() const; // returns the average song length in minutes (totalMinutes_ / numSongs_). If numSongs_ is 0, return 0.
+	double averageSongLength() const; // returns the average song length in minutes (totalMinutes_ / numSongs_). If numSongs_ is 0, return 0.
 
 	Playlist operator+(const Playlist& other) const; //Returns a new Playlist whose numSongs_ and totalMinutes_ are the sum of the two operands.
 
